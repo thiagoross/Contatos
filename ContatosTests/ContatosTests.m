d086e587c354dc15d8380abccdd527432a7af11e
Thiago <thiago@Thiagos-iMac.local>
2015-03-21 19:35:28 -0300

    Initial Commit

---
+++ ContatosTests/ContatosTests.m
@@ -0,0 +1,40 @@
+//
+//  ContatosTests.m
+//  ContatosTests
+//
+//  Created by Thiago on 3/21/15.
+//  Copyright (c) 2015 Rossener. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ContatosTests : XCTestCase
+
+@end
+
+@implementation ContatosTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
